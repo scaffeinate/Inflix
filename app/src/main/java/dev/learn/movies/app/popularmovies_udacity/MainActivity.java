31eb393ad680a9d9de80440c01b3be02da4be9b1
Sudharsanan <sudhar.ti@gmail.com>
2017-10-06 19:57:57 -0400

    Init project

---
+++ app/src/main/java/dev/learn/movies/app/popularmovies_udacity/MainActivity.java
@@ -0,0 +1,13 @@
+package dev.learn.movies.app.popularmovies_udacity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
