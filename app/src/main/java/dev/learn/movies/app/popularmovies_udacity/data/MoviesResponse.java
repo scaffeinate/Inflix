79802e511ee44d7e1f2741cddb1044250e6c57c3
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-10-10 03:03:42 -0400

    Add gson, pojo classes for the response

---
+++ app/src/main/java/dev/learn/movies/app/popularmovies_udacity/data/MoviesResponse.java
@@ -0,0 +1,55 @@
+
+package dev.learn.movies.app.popularmovies_udacity.data;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class MoviesResponse {
+
+    @SerializedName("page")
+    @Expose
+    private long page;
+    @SerializedName("total_results")
+    @Expose
+    private long totalResults;
+    @SerializedName("total_pages")
+    @Expose
+    private long totalPages;
+    @SerializedName("results")
+    @Expose
+    private List<Result> results = null;
+
+    public long getPage() {
+        return page;
+    }
+
+    public void setPage(long page) {
+        this.page = page;
+    }
+
+    public long getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(long totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public long getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(long totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+}
