2a4eb062a179a8e630af84c1e999d39cfeb0dd2c
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-10-11 03:16:59 -0400

    Add DetailActivity

---
+++ app/src/main/java/dev/learn/movies/app/popularmovies_udacity/DetailActivity.java
@@ -0,0 +1,16 @@
+package dev.learn.movies.app.popularmovies_udacity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sudharti on 10/11/17.
+ */
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
