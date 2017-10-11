875b95dd953e0eee1f25beca800c4fd16edc9b18
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-10-11 01:31:27 -0400

    Cleanup MoviesAdapter and layout

--- app/src/main/java/dev/learn/movies/app/popularmovies_udacity/MoviesAdapter.java
+++ app/src/main/java/dev/learn/movies/app/popularmovies_udacity/MoviesAdapter.java
@@ -1,16 +1,34 @@
 package dev.learn.movies.app.popularmovies_udacity;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.learn.movies.app.popularmovies_udacity.common.Movie;
+import dev.learn.movies.app.popularmovies_udacity.network.HTTPHelper;
+
 /**
  * Created by sudharti on 10/10/17.
  */
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+
+    private List<Movie> movieList;
+    private Context mContext;
+
+    public MoviesAdapter(Context context) {
+        this.mContext = context;
+        this.movieList = new ArrayList<>();
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_grid, null);
@@ -19,12 +37,17 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movieList.size();
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bind(position);
+    }
 
+    public void setMovieList(List<Movie> movieList) {
+        this.movieList = movieList;
+        notifyDataSetChanged();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
@@ -32,11 +55,14 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
 
         public ViewHolder(View itemView) {
             super(itemView);
-            mPosterImageView = (ImageView) itemView.findViewById(R.id.imageview_poster);
+            mPosterImageView = (ImageView) itemView;
         }
 
         private void bind(int position) {
-
+            Movie movie = movieList.get(position);
+            Picasso.with(mContext)
+                    .load(HTTPHelper.buildImageResourceUri(movie.getPosterPath()))
+                    .into(mPosterImageView);
         }
     }
 }
