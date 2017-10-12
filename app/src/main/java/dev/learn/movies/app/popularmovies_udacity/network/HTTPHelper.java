3373d5dc4883c370fca64e24b517cc4777f320cf
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-10-11 23:38:55 -0400

    Added HTTPHelper

---
+++ app/src/main/java/dev/learn/movies/app/popularmovies_udacity/network/HTTPHelper.java
@@ -0,0 +1,126 @@
+package dev.learn.movies.app.popularmovies_udacity.network;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by sudharti on 10/10/17.
+ */
+
+public final class HTTPHelper {
+
+    private final static String TAG = HTTPHelper.class.getSimpleName();
+
+    private final static String API_KEY = "API_KEY";
+
+    private final static String SCHEME = "https";
+
+    private final static String BASE_PATH = "/api.themoviedb.org";
+
+    private final static String API_VERSION = "3";
+
+    private final static String DISCOVER_PATH = "discover/movie";
+
+    private final static String IMAGE_BASE_PATH = "/image.tmdb.org/t/p/";
+
+    private final static String IMAGE_SIZE = "w342";
+
+    private final static String API_KEY_PARAM = "api_key";
+
+    private final static String MOST_POPULAR_PATH = "movie/popular";
+
+    private final static String TOP_RATED_PATH = "movie/top_rated";
+
+    private final static String LANGUAGE = "language";
+
+    private final static String EN_US = "en-US";
+
+    public static URL buildDiscoverURL() {
+        Uri uri = new Uri.Builder()
+                .scheme(SCHEME)
+                .appendEncodedPath(BASE_PATH)
+                .appendEncodedPath(API_VERSION)
+                .appendEncodedPath(DISCOVER_PATH)
+                .appendQueryParameter(API_KEY_PARAM, API_KEY)
+                .appendQueryParameter(LANGUAGE, EN_US)
+                .build();
+        try {
+            return new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        return null;
+    }
+
+    public static URL buildMostPopularURL() {
+        Uri uri = new Uri.Builder()
+                .scheme(SCHEME)
+                .appendEncodedPath(BASE_PATH)
+                .appendEncodedPath(API_VERSION)
+                .appendEncodedPath(MOST_POPULAR_PATH)
+                .appendQueryParameter(API_KEY_PARAM, API_KEY)
+                .appendQueryParameter(LANGUAGE, EN_US)
+                .build();
+
+        try {
+            return new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        return null;
+    }
+
+    public static URL builTopRatedURL() {
+        Uri uri = new Uri.Builder()
+                .scheme(SCHEME)
+                .appendEncodedPath(BASE_PATH)
+                .appendEncodedPath(API_VERSION)
+                .appendEncodedPath(TOP_RATED_PATH)
+                .appendQueryParameter(API_KEY_PARAM, API_KEY)
+                .appendQueryParameter(LANGUAGE, EN_US)
+                .build();
+        try {
+            return new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        return null;
+    }
+
+    public static Uri buildImageResourceUri(String imgFile) {
+        return new Uri.Builder()
+                .scheme(SCHEME)
+                .appendEncodedPath(IMAGE_BASE_PATH)
+                .appendEncodedPath(IMAGE_SIZE)
+                .appendEncodedPath(imgFile)
+                .build();
+    }
+
+    public static String getHTTPResponse(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        String response = null;
+        try {
+            InputStream in = connection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            if (scanner.hasNext()) {
+                response = scanner.next();
+            }
+            scanner.close();
+        } finally {
+            connection.disconnect();
+        }
+        return response;
+    }
+}
