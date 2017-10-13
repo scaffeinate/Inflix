8c59fa3f1ebb60ee63c3b623c491a0b74f81fd78
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-10-13 00:10:55 -0400

    Finish up DetailActivity, populate movie details view

--- app/src/main/java/dev/learn/movies/app/popularmovies_udacity/DetailActivity.java
+++ app/src/main/java/dev/learn/movies/app/popularmovies_udacity/DetailActivity.java
@@ -1,27 +1,27 @@
 package dev.learn.movies.app.popularmovies_udacity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
-import com.squareup.picasso.Picasso;
 
 import dev.learn.movies.app.popularmovies_udacity.common.MovieDetail;
 import dev.learn.movies.app.popularmovies_udacity.network.HTTPHelper;
 import dev.learn.movies.app.popularmovies_udacity.network.NetworkTask;
 import dev.learn.movies.app.popularmovies_udacity.network.NetworkTaskCallback;
+import dev.learn.movies.app.popularmovies_udacity.util.DisplayUtils;
 
 /**
  * Created by sudharti on 10/11/17.
@@ -40,8 +40,16 @@ public class DetailActivity extends AppCompatActivity implements NetworkTaskCall
     private LinearLayout mMovieDetailLayout;
     private ProgressBar mProgressBar;
     private TextView mErrorMessageDisplay;
+
     private ImageView mBackdropImageView;
     private ImageView mPosterImageView;
+    private TextView mMovieTitleTextView;
+    private TextView mMovieRuntimeTextView;
+    private TextView mMovieGenreTextView;
+    private TextView mMovieRatingTextView;
+    private TextView mMoviePlotTextView;
+
+    private RatingBar mMovieRatingBar;
 
     private final Gson gson = new Gson();
 
@@ -57,8 +65,15 @@ public class DetailActivity extends AppCompatActivity implements NetworkTaskCall
         mMovieDetailLayout = (LinearLayout) findViewById(R.id.layout_movie_detail);
         mProgressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
         mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+
         mBackdropImageView = (ImageView) findViewById(R.id.imageview_backdrop);
         mPosterImageView = (ImageView) findViewById(R.id.imageview_poster);
+        mMovieTitleTextView = (TextView) findViewById(R.id.tv_movie_title);
+        mMovieRuntimeTextView = (TextView) findViewById(R.id.tv_movie_runtime);
+        mMovieGenreTextView = (TextView) findViewById(R.id.tv_movie_genre);
+        mMovieRatingTextView = (TextView) findViewById(R.id.tv_movie_rating);
+        mMoviePlotTextView = (TextView) findViewById(R.id.tv_movie_plot);
+        mMovieRatingBar = (RatingBar) findViewById(R.id.rb_movie_rating);
 
         setSupportActionBar(mToolbar);
         mActionBar = getSupportActionBar();
@@ -125,14 +140,35 @@ public class DetailActivity extends AppCompatActivity implements NetworkTaskCall
         String backdropURL = movieDetail.getBackdropPath();
         String posterURL = movieDetail.getPosterPath();
 
-        Picasso.with(this)
-                .load(HTTPHelper.buildImageResourceUri(backdropURL, HTTPHelper.IMAGE_SIZE_XLARGE))
-                .fit().centerCrop()
-                .into(mBackdropImageView);
+        if (backdropURL != null) {
+            Uri backdropUri = HTTPHelper.buildImageResourceUri(backdropURL, HTTPHelper.IMAGE_SIZE_XLARGE);
+            DisplayUtils.fitImageInto(mBackdropImageView, backdropUri, null);
+        }
 
-        Picasso.with(this).load(HTTPHelper.buildImageResourceUri(posterURL, HTTPHelper.IMAGE_SIZE_SMALL))
-                .fit().centerCrop()
-                .into(mPosterImageView);
+        if (posterURL != null) {
+            Uri posterUri = HTTPHelper.buildImageResourceUri(posterURL, HTTPHelper.IMAGE_SIZE_SMALL);
+            DisplayUtils.fitImageInto(mPosterImageView, posterUri, null);
+        }
+
+        StringBuilder titleBuilder = new StringBuilder();
+        if (movieDetail.getTitle() != null) {
+            titleBuilder.append(movieDetail.getTitle());
+        }
+        int year = DisplayUtils.getYear(movieDetail.getReleaseDate(), "yyyy-mm-dd");
+        if (year != -1) {
+            titleBuilder.append(" ").append("(").append(String.valueOf(year)).append(")");
+        }
+
+        mMovieTitleTextView.setText(titleBuilder.toString());
+        mMovieRuntimeTextView.setText(new StringBuilder().append(movieDetail.getRuntime()).append(" ").append("min").toString());
+
+        double rating = movieDetail.getVoteAverage();
+        mMovieRatingBar.setRating((float) rating);
+        mMovieRatingTextView.setText(new StringBuilder().append(rating).append("/").append("10").toString());
+
+        if (movieDetail.getOverview() != null) {
+            mMoviePlotTextView.setText(movieDetail.getOverview());
+        }
     }
 
     private void showProgressBar() {
