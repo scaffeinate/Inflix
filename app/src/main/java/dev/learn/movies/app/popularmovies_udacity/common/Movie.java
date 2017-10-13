535ab90a11d272f2865a50f80638ca3454d6b06d
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-10-13 01:25:41 -0400

    Cleanup unwanted fields from POJO classes

--- app/src/main/java/dev/learn/movies/app/popularmovies_udacity/common/Movie.java
+++ app/src/main/java/dev/learn/movies/app/popularmovies_udacity/common/Movie.java
@@ -1,165 +1,34 @@
 
 package dev.learn.movies.app.popularmovies_udacity.common;
 
-import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Movie {
 
-    @SerializedName("vote_count")
-    @Expose
-    private long voteCount;
+
     @SerializedName("id")
     @Expose
     private long id;
-    @SerializedName("video")
-    @Expose
-    private boolean video;
-    @SerializedName("vote_average")
-    @Expose
-    private double voteAverage;
+
     @SerializedName("title")
     @Expose
     private String title;
-    @SerializedName("popularity")
-    @Expose
-    private double popularity;
+
     @SerializedName("poster_path")
     @Expose
     private String posterPath;
-    @SerializedName("original_language")
-    @Expose
-    private String originalLanguage;
-    @SerializedName("original_title")
-    @Expose
-    private String originalTitle;
-    @SerializedName("genre_ids")
-    @Expose
-    private List<Long> genreIds = null;
-    @SerializedName("backdrop_path")
-    @Expose
-    private String backdropPath;
-    @SerializedName("adult")
-    @Expose
-    private boolean adult;
-    @SerializedName("overview")
-    @Expose
-    private String overview;
-    @SerializedName("release_date")
-    @Expose
-    private String releaseDate;
-
-    public long getVoteCount() {
-        return voteCount;
-    }
-
-    public void setVoteCount(long voteCount) {
-        this.voteCount = voteCount;
-    }
 
     public long getId() {
         return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public boolean isVideo() {
-        return video;
-    }
-
-    public void setVideo(boolean video) {
-        this.video = video;
-    }
-
-    public double getVoteAverage() {
-        return voteAverage;
-    }
-
-    public void setVoteAverage(double voteAverage) {
-        this.voteAverage = voteAverage;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public double getPopularity() {
-        return popularity;
-    }
-
-    public void setPopularity(double popularity) {
-        this.popularity = popularity;
-    }
-
     public String getPosterPath() {
         return posterPath;
-    }
-
-    public void setPosterPath(String posterPath) {
-        this.posterPath = posterPath;
-    }
-
-    public String getOriginalLanguage() {
-        return originalLanguage;
-    }
-
-    public void setOriginalLanguage(String originalLanguage) {
-        this.originalLanguage = originalLanguage;
-    }
-
-    public String getOriginalTitle() {
-        return originalTitle;
-    }
-
-    public void setOriginalTitle(String originalTitle) {
-        this.originalTitle = originalTitle;
-    }
-
-    public List<Long> getGenreIds() {
-        return genreIds;
-    }
-
-    public void setGenreIds(List<Long> genreIds) {
-        this.genreIds = genreIds;
-    }
-
-    public String getBackdropPath() {
-        return backdropPath;
-    }
-
-    public void setBackdropPath(String backdropPath) {
-        this.backdropPath = backdropPath;
-    }
-
-    public boolean isAdult() {
-        return adult;
-    }
-
-    public void setAdult(boolean adult) {
-        this.adult = adult;
-    }
-
-    public String getOverview() {
-        return overview;
-    }
-
-    public void setOverview(String overview) {
-        this.overview = overview;
-    }
-
-    public String getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(String releaseDate) {
-        this.releaseDate = releaseDate;
     }
 
 }
--- app/src/main/java/dev/learn/movies/app/popularmovies_udacity/common/MovieDetail.java
+++ app/src/main/java/dev/learn/movies/app/popularmovies_udacity/common/MovieDetail.java
@@ -8,66 +8,43 @@ import java.util.List;
 
 public class MovieDetail {
 
-    @SerializedName("adult")
-    @Expose
-    private boolean adult;
+
     @SerializedName("backdrop_path")
     @Expose
     private String backdropPath;
-    @SerializedName("belongs_to_collection")
-    @Expose
-    private Object belongsToCollection;
-    @SerializedName("budget")
-    @Expose
-    private long budget;
-    @SerializedName("homepage")
-    @Expose
-    private String homepage;
+
     @SerializedName("id")
     @Expose
     private long id;
-    @SerializedName("imdb_id")
-    @Expose
-    private String imdbId;
-    @SerializedName("original_language")
-    @Expose
-    private String originalLanguage;
-    @SerializedName("original_title")
-    @Expose
-    private String originalTitle;
+
     @SerializedName("overview")
     @Expose
     private String overview;
-    @SerializedName("popularity")
-    @Expose
-    private double popularity;
+
     @SerializedName("poster_path")
     @Expose
     private String posterPath;
+
     @SerializedName("release_date")
     @Expose
     private String releaseDate;
-    @SerializedName("revenue")
-    @Expose
-    private long revenue;
+
     @SerializedName("runtime")
     @Expose
     private long runtime;
-    @SerializedName("status")
-    @Expose
-    private String status;
+
     @SerializedName("tagline")
     @Expose
     private String tagline;
+
     @SerializedName("title")
     @Expose
     private String title;
-    @SerializedName("video")
-    @Expose
-    private boolean video;
+
     @SerializedName("vote_average")
     @Expose
     private double voteAverage;
+
     @SerializedName("vote_count")
     @Expose
     private long voteCount;
@@ -76,44 +53,12 @@ public class MovieDetail {
     @Expose
     private List<Genre> genres;
 
-    public boolean isAdult() {
-        return adult;
-    }
-
-    public void setAdult(boolean adult) {
-        this.adult = adult;
-    }
-
     public String getBackdropPath() {
         return backdropPath;
     }
 
     public void setBackdropPath(String backdropPath) {
         this.backdropPath = backdropPath;
-    }
-
-    public Object getBelongsToCollection() {
-        return belongsToCollection;
-    }
-
-    public void setBelongsToCollection(Object belongsToCollection) {
-        this.belongsToCollection = belongsToCollection;
-    }
-
-    public long getBudget() {
-        return budget;
-    }
-
-    public void setBudget(long budget) {
-        this.budget = budget;
-    }
-
-    public String getHomepage() {
-        return homepage;
-    }
-
-    public void setHomepage(String homepage) {
-        this.homepage = homepage;
     }
 
     public long getId() {
@@ -124,44 +69,12 @@ public class MovieDetail {
         this.id = id;
     }
 
-    public String getImdbId() {
-        return imdbId;
-    }
-
-    public void setImdbId(String imdbId) {
-        this.imdbId = imdbId;
-    }
-
-    public String getOriginalLanguage() {
-        return originalLanguage;
-    }
-
-    public void setOriginalLanguage(String originalLanguage) {
-        this.originalLanguage = originalLanguage;
-    }
-
-    public String getOriginalTitle() {
-        return originalTitle;
-    }
-
-    public void setOriginalTitle(String originalTitle) {
-        this.originalTitle = originalTitle;
-    }
-
     public String getOverview() {
         return overview;
     }
 
     public void setOverview(String overview) {
         this.overview = overview;
-    }
-
-    public double getPopularity() {
-        return popularity;
-    }
-
-    public void setPopularity(double popularity) {
-        this.popularity = popularity;
     }
 
     public String getPosterPath() {
@@ -180,28 +93,12 @@ public class MovieDetail {
         this.releaseDate = releaseDate;
     }
 
-    public long getRevenue() {
-        return revenue;
-    }
-
-    public void setRevenue(long revenue) {
-        this.revenue = revenue;
-    }
-
     public long getRuntime() {
         return runtime;
     }
 
     public void setRuntime(long runtime) {
         this.runtime = runtime;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 
     public String getTagline() {
@@ -218,14 +115,6 @@ public class MovieDetail {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public boolean isVideo() {
-        return video;
-    }
-
-    public void setVideo(boolean video) {
-        this.video = video;
     }
 
     public double getVoteAverage() {
