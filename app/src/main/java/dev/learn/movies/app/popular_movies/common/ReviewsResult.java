657dc2ec0b64cfeeb0c1c5aac076a2497f0be2dd
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-11-04 01:31:57 -0400

    Add Reviews POJO objects

---
+++ app/src/main/java/dev/learn/movies/app/popular_movies/common/ReviewsResult.java
@@ -0,0 +1,67 @@
+
+package dev.learn.movies.app.popular_movies.common;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ReviewsResult {
+
+    @SerializedName("id")
+    @Expose
+    private long id;
+    @SerializedName("page")
+    @Expose
+    private long page;
+    @SerializedName("results")
+    @Expose
+    private List<Review> results = null;
+    @SerializedName("total_pages")
+    @Expose
+    private long totalPages;
+    @SerializedName("total_results")
+    @Expose
+    private long totalResults;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPage() {
+        return page;
+    }
+
+    public void setPage(long page) {
+        this.page = page;
+    }
+
+    public List<Review> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Review> results) {
+        this.results = results;
+    }
+
+    public long getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(long totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public long getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(long totalResults) {
+        this.totalResults = totalResults;
+    }
+
+}
