bec9b306e3247dac66951c6e82955e4763d13bf8
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-11-04 18:38:09 -0400

    Finish up Content provider

--- app/src/main/java/dev/learn/movies/app/popular_movies/data/FavoriteContentProvider.java
+++ app/src/main/java/dev/learn/movies/app/popular_movies/data/FavoriteContentProvider.java
@@ -1,13 +1,20 @@
 package dev.learn.movies.app.popular_movies.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
+import java.util.List;
+
+import static dev.learn.movies.app.popular_movies.data.DataContract.FavoriteEntry;
 
 /**
  * Created by sudharti on 11/4/17.
@@ -40,7 +47,22 @@ public class FavoriteContentProvider extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+        final SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        int match = sUriMatcher.match(uri);
+        Cursor cursor;
+        switch (match) {
+            case FAVORITES:
+                cursor = db.query(FavoriteEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            case FAVORITE_WITH_MOVIE_ID:
+                cursor = db.query(FavoriteEntry.TABLE_NAME, projection, FavoriteEntry.COLUMN_MOVIE_ID + " = ?", selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        mContext.getContentResolver().notifyChange(uri, null);
+        return cursor;
     }
 
     @Nullable
@@ -52,12 +74,40 @@ public class FavoriteContentProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+        final SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        int match = sUriMatcher.match(uri);
+        long res;
+        switch (match) {
+            case FAVORITES:
+                res = db.insert(FavoriteEntry.TABLE_NAME, null, values);
+                if (res == -1) {
+                    throw new SQLiteException("Failed to insert record");
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        mContext.getContentResolver().notifyChange(uri, null);
+        return ContentUris.withAppendedId(uri, res);
     }
 
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+        final SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        int match = sUriMatcher.match(uri), res = 0;
+        switch (match) {
+            case FAVORITE_WITH_MOVIE_ID:
+                List<String> pathSegments = uri.getPathSegments();
+                if (pathSegments != null && pathSegments.size() > 1) {
+                    String id = uri.getPathSegments().get(1);
+                    res = db.delete(FavoriteEntry.TABLE_NAME, FavoriteEntry.COLUMN_MOVIE_ID + " = ? ", new String[]{id});
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri : " + uri);
+        }
+        mContext.getContentResolver().notifyChange(uri, null);
+        return res;
     }
 
     @Override
