5370eb91cb166cfc126885e047c155c0f256fc87
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-11-05 15:20:22 -0500

    Fix layout_user_reviews

--- app/src/main/java/dev/learn/movies/app/popular_movies/adapters/MovieReviewsAdapter.java
+++ app/src/main/java/dev/learn/movies/app/popular_movies/adapters/MovieReviewsAdapter.java
@@ -26,7 +26,7 @@ public class MovieReviewsAdapter extends RecyclerView.Adapter<MovieReviewsAdapte
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_review_card, null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user_review, null);
         return new ViewHolder(view);
     }
 
