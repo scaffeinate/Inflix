c33b873874dbe026872a0ef0678296be984e2a64
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-11-06 20:39:13 -0500

    Loader pagination

--- app/src/main/java/dev/learn/movies/app/popular_movies/adapters/MoviesAdapter.java
+++ app/src/main/java/dev/learn/movies/app/popular_movies/adapters/MoviesAdapter.java
@@ -1,11 +1,14 @@
 package dev.learn.movies.app.popular_movies.adapters;
 
 import android.net.Uri;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,10 +22,14 @@ import dev.learn.movies.app.popular_movies.util.DisplayUtils;
  * MoviesAdapter - RecyclerView Adapter for Movies
  */
 
-public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+public class MoviesAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+    private static final int VIEWTYPE_LOADING = 1;
+    private static final int VIEWTYPE_CONTENT = 2;
 
     private final OnItemClickHandler mHandler;
     private List<Movie> movieList;
+    private boolean mShowLoading = false;
 
     public MoviesAdapter(OnItemClickHandler handler) {
         this.mHandler = handler;
@@ -32,26 +39,45 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_poster, parent, false);
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        if (viewType == VIEWTYPE_CONTENT) {
+            View view = layoutInflater.inflate(R.layout.item_poster, parent, false);
 
         /* Set the minimum height of the view to scale for all screens.
          *
          * Reference: https://stackoverflow.com/questions/35221566/how-to-set-the-height-of-an-item-row-in-gridlayoutmanager
          */
-        int height = parent.getMeasuredHeight() / 2;
-        ViewHolder viewHolder = new ViewHolder(view);
-        viewHolder.mPosterImageView.setMinimumHeight(height);
-        return viewHolder;
+            int height = parent.getMeasuredHeight() / 2;
+            ContentViewHolder viewHolder = new ContentViewHolder(view);
+            viewHolder.mPosterImageView.setMinimumHeight(height);
+            return viewHolder;
+        } else {
+            View view = layoutInflater.inflate(R.layout.item_footer_loader, parent, false);
+            return new LoaderViewHolder(view);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return movieList.size();
+        return movieList.size() + 1;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.bind(position);
+        if (holder instanceof LoaderViewHolder) {
+            ((LoaderViewHolder) holder).bind();
+        } else if (holder instanceof ContentViewHolder) {
+            ((ContentViewHolder) holder).bind(position);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return VIEWTYPE_LOADING;
+        }
+
+        return VIEWTYPE_CONTENT;
     }
 
     public void setMovieList(List<Movie> movieList) {
@@ -59,11 +85,15 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
         notifyDataSetChanged();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public void showLoading(boolean showLoading) {
+        this.mShowLoading = showLoading;
+    }
+
+    class ContentViewHolder extends ViewHolder implements View.OnClickListener {
 
         private final ImageView mPosterImageView;
 
-        public ViewHolder(View itemView) {
+        public ContentViewHolder(View itemView) {
             super(itemView);
             mPosterImageView = itemView.findViewById(R.id.image_view_poster);
             mPosterImageView.setOnClickListener(this);
@@ -87,6 +117,24 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
         public void onClick(View view) {
             int position = getAdapterPosition();
             mHandler.onClick(position);
+        }
+    }
+
+    class LoaderViewHolder extends ViewHolder {
+
+        private final ProgressBar mProgressBar;
+
+        public LoaderViewHolder(View itemView) {
+            super(itemView);
+            mProgressBar = itemView.findViewById(R.id.pb_footer_loading);
+        }
+
+        private void bind() {
+            if (mShowLoading) {
+                itemView.setVisibility(View.VISIBLE);
+            } else {
+                itemView.setVisibility(View.GONE);
+            }
         }
     }
 }
