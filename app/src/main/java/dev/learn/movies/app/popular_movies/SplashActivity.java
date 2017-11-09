e0940493c2792a80b7d60b4dee3c9376a0834413
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-11-08 23:50:45 -0500

    Add SVG assets, splash activity

---
+++ app/src/main/java/dev/learn/movies/app/popular_movies/SplashActivity.java
@@ -0,0 +1,17 @@
+package dev.learn.movies.app.popular_movies;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sudharti on 11/8/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
