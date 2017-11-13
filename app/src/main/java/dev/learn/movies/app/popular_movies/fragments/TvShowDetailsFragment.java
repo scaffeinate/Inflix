f77181a775d001f37e9301e2e5c5a5b0311deb64
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-11-13 02:40:08 -0500

    Add TvShowDetailsFragment

---
+++ app/src/main/java/dev/learn/movies/app/popular_movies/fragments/TvShowDetailsFragment.java
@@ -0,0 +1,10 @@
+package dev.learn.movies.app.popular_movies.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by sudharti on 11/13/17.
+ */
+
+public class TvShowDetailsFragment extends Fragment {
+}
